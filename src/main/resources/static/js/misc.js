fcf3c638807d619bbec3a3f46f8e675683a5fe2f
Cramcat639 <marc639@outlook.com>
2024-02-23 15:45:18 +0100

    1.2.0.2
    Merch Check Point

--- src/main/resources/static/js/misc.js
+++ src/main/resources/static/js/misc.js
@@ -2,51 +2,52 @@
   'use strict';
   $(function() {
     var body = $('body');
-    var contentWrapper = $('.content-wrapper');
-    var scroller = $('.container-scroller');
-    var footer = $('.footer');
     var sidebar = $('.sidebar');
 
     // Agrega la clase activa a nav-link basado en la URL de manera dinámica
     // La clase activa también puede ser codificada directamente en el archivo HTML según sea necesario
 
     function addActiveClass(element) {
-      var href = element.attr('href');
-
-      // Para la url root (index.html)
-      if (current === "" && href.endsWith("index.html")) {
-        element.parents('.nav-item').last().addClass('active');
-        if (element.parents('.sub-menu').length) {
-          element.closest('.collapse').addClass('show');
-          element.addClass('active');
+      console.log(current);
+      if (current == "users") {
+        //for root url
+        if (element.attr('href').indexOf("index.html") !== -1) {
+          element.parents('.nav-item').last().addClass('active');
+          console.log("if1");
+          if (element.parents('.sub-menu').length) {
+            element.closest('.collapse').addClass('show');
+            element.addClass('active');
+            console.log("if");
+          }
         }
-      }
-
-      // Para otras url
-      else if (href.endsWith(current)) {
-        element.parents('.nav-item').last().addClass('active');
-        if (element.parents('.sub-menu').length) {
-          element.closest('.collapse').addClass('show');
-          element.addClass('active');
-        }
-        if (element.parents('.submenu-item').length) {
-          element.addClass('active');
+      } else {
+        //for other url
+        console.log("else");
+        if (element.attr('href').indexOf(current) !== -1) {
+          element.parents('.nav-item').last().addClass('active');
+          if (element.parents('.sub-menu').length) {
+            element.closest('.collapse').addClass('show');
+            element.addClass('active');
+          }
+          if (element.parents('.submenu-item').length) {
+            element.addClass('active');
+          }
         }
       }
     }
-
-    var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
+    
+    var current = location.pathname.split("/").slice(-2)[0].replace(/^\/|\/$/g, '');
     $('.nav li a', sidebar).each(function() {
       var $this = $(this);
       addActiveClass($this);
     })
-
+/*
     $('.horizontal-menu .nav li a').each(function() {
       var $this = $(this);
       addActiveClass($this);
     })
-
-    // Cierra otros submenús en la barra lateral al abrir alguno
+*/
+    //Close other submenu in sidebar on opening any
 
     sidebar.on('show.bs.collapse', '.collapse', function() {
       sidebar.find('.collapse.show').collapse('hide');
